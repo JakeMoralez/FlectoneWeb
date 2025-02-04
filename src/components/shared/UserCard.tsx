ad632e1a418dc71204cbfdaea006d2504b491ac4
awalki <je3nsoff@icloud.com>
2025-02-04 11:37:58 -0800

    main: improve mobile support eg: navbar, about, chat, mix

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -14,17 +14,23 @@ interface Props {
 
 export default function UserCard({ name, about }: Props) {
   return (
-    <Card className="dark:bg-neutral-800/50 w-1/3">
-      <a href={`https://github.com/${name}`} rel="noopener noreferrer">
-        <CardHeader className="flex gap-6">
+    <Card className="dark:bg-neutral-800/50 w-full h-[200px] transition-transform hover:scale-105">
+      <a
+        href={`https://github.com/${name}`}
+        rel="noopener noreferrer"
+        className="h-full"
+      >
+        <CardHeader className="flex gap-6 pb-2">
           <Avatar>
             <AvatarImage src={`https://github.com/${name}.png`} alt="logo" />
             <AvatarFallback>{name} avatar</AvatarFallback>
           </Avatar>
-          <CardTitle className="font-montserrat">{name}</CardTitle>
+          <CardTitle className="font-montserrat text-lg">{name}</CardTitle>
         </CardHeader>
         <CardContent>
-          <CardDescription className="font-inter">{about}</CardDescription>
+          <CardDescription className="font-inter text-sm line-clamp-4">
+            {about}
+          </CardDescription>
         </CardContent>
       </a>
     </Card>
