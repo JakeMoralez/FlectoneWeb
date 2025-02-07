cc3b35dbb20dbd903cadde857c73c9732af11590
awalki <je3nsoff@icloud.com>
2025-02-07 05:27:56 -0800

    remove 3.gif

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -1,7 +1,5 @@
 import firstImg from "@/assets/chat/1.gif";
 import secondImg from "@/assets/chat/2.gif";
-import thirdImg from "@/assets/chat/3.gif";
-import fourthImg from "@/assets/chat/4.gif";
 import fifthImg from "@/assets/chat/5.gif";
 import sixthImg from "@/assets/chat/6.gif";
 import { Card, CardContent } from "@/components/ui/card";
@@ -17,8 +15,6 @@ export function CarouselPlugin() {
   const carouselItems = [
     { image: firstImg, caption: "Chat bubble" },
     { image: secondImg, caption: "Info about item in chat" },
-    { image: thirdImg, caption: "Spoiler" },
-    { image: fourthImg, caption: "Custom chat colors" },
     { image: fifthImg, caption: "Item signing" },
     { image: sixthImg, caption: "Marks" },
   ];
